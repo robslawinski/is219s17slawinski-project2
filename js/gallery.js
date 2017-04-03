158bb3769ce648d08758a1936bbf46ce140d3b95
robslawinski <rslawinski@verizon.net>
2017-04-02 21:52:08 -0400

    GET PARAM

--- js/gallery.js
+++ js/gallery.js
@@ -46,6 +46,10 @@ function getQueryParams(qs) {
 
 var $_GET = getQueryParams(document.location.search);
 console.log( "$_GET: " + $_GET["json"]); 
+if($_GET["json"] != undefined)
+{
+	mUrl = $_GET["json"];
+}
 
 function swapPhoto() {
 	//Add code here to access the #slideShow element.
